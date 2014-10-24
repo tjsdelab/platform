894639f64c5884d9d0f6cf58b5f7f7b2d22aa477
qilong.yin <qilong.yin@spreadtrum.com>
2014-10-24 17:08:20 +0800

    merge

--- src/com/spreadtrum/action/IndexPageAction.java
+++ src/com/spreadtrum/action/IndexPageAction.java
@@ -1,8 +1,5 @@
 package com.spreadtrum.action;
 
-import java.sql.Date;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,12 +8,7 @@ import org.apache.struts2.ServletActionContext;
 import com.opensymphony.xwork2.ActionSupport;
 import com.spreadtrum.model.*;
 import com.spreadtrum.sanity_smoke.dao.SanityProjectDAO;
-import com.spreadtrum.sanity_smoke.dao.SmokeProjectDAO;
-import com.spreadtrum.sanity_smoke.dao.SmokeTestFormDAO;
 import com.spreadtrum.sanity_smoke.dao.impl.SanityProjectDAOImpl;
-import com.spreadtrum.sanity_smoke.dao.impl.SmokeProjectDAOImpl;
-import com.spreadtrum.sanity_smoke.dao.impl.SmokeTestFormDAOImpl;
-import com.spreadtrum.sanity_smoke.model.SmokeTestInfo;
 import com.spreadtrum.dao.*;
 import com.spreadtrum.dao.impl.*;
 
@@ -26,8 +18,6 @@ public class IndexPageAction extends ActionSupport {
 	private IndexPageDAO indexPageDAO = new IndexPageDAOImpl();
 	private NewsDAO newsDAO = new NewsDAOImpl();
 	private TestInfoDAO testInfoDAO = new TestInfoDAOImpl();
-	private SanityProjectDAO SmokeProjectDAO = new SanityProjectDAOImpl();
-	//private SanityTestInfo sanityTestInfo = new SanityTestInfo();
 	
 
 	//向project.action传递的formname，通过formName方法
@@ -72,10 +62,8 @@ public class IndexPageAction extends ActionSupport {
 	}
 	public String execute(){
 
-		//sanityTestInfo = sanityProjectDAO.getSanityValidProjectName().get(0);
 
 
-        System.out.println("action print" + SmokeProjectDAO.getSanityValidProjectName());
 		
 	IndexPage indexPage=new IndexPage();
 	
